4571cb42c82ddcdb9b85a1ad9d522f8455282ce3
kacpermajcher <kacpermajcher13@gmail.com>
2024-04-10 10:09:20 +0200

    Create video controls

--- lib/features/presentation/pages/widgets/channel_player.dart
+++ lib/features/presentation/pages/widgets/channel_player.dart
@@ -30,10 +30,12 @@ In the case of a mobile application, display the decoded video.
 
 class ChannelPlayerState extends State<ChannelPlayer> {
   late VideoPlayerController _controller;
+  bool _fullScreen = false;
 
   @override
   void initState() {
     super.initState();
+
     _controller = VideoPlayerController.networkUrl(url);
 
     _controller.addListener(() {
@@ -46,22 +48,16 @@ class ChannelPlayerState extends State<ChannelPlayer> {
     log(kIsWeb
         ? 'Welcome in the web, I\'ll play a link with a browser-supported format.'
         : 'Welcome to the app, I have a specially decoded video for you.');
-        
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    final double dh = MediaQuery.of(context).size.height;
     return Scaffold(
       body: ListView(
         children: <Widget>[
           Container(
-            padding: const EdgeInsets.only(top: 50.0),
+            padding: EdgeInsets.only(top: dh * .05),
           ),
           AspectRatio(
             aspectRatio: _controller.value.aspectRatio,
@@ -69,12 +65,115 @@ class ChannelPlayerState extends State<ChannelPlayer> {
               alignment: Alignment.bottomCenter,
               children: <Widget>[
                 VideoPlayer(_controller),
-                VideoProgressIndicator(_controller, allowScrubbing: true),
+                VideoProgressIndicator(
+                  _controller,
+                  allowScrubbing: true,
+                ),
+                VideoControls(
+                  controller: _controller,
+                  isFullScreen: _fullScreen,
+                  toggleFullScreen: () {
+                    setState(() {
+                      _fullScreen = !_fullScreen;
+                    });
+
+                    if (_fullScreen) {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          fullscreenDialog: true,
+                          builder: (context) => Scaffold(
+                            body: Center(
+                              child: AspectRatio(
+                                aspectRatio: _controller.value.aspectRatio,
+                                child: Stack(
+                                  alignment: Alignment.bottomCenter,
+                                  children: <Widget>[
+                                    VideoPlayer(_controller),
+                                    VideoProgressIndicator(
+                                      _controller,
+                                      allowScrubbing: true,
+                                    ),
+                                    VideoControls(
+                                      controller: _controller,
+                                      isFullScreen: _fullScreen,
+                                      toggleFullScreen: () {
+                                        setState(() {
+                                          _fullScreen = !_fullScreen;
+                                        });
+                                        Navigator.pop(context);
+                                      },
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      );
+                    } else {
+                      Navigator.pop(context);
+                    }
+                  },
+                ),
               ],
             ),
           ),
         ],
       ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+}
+
+class VideoControls extends StatelessWidget {
+  final VideoPlayerController controller;
+  final bool isFullScreen;
+  final VoidCallback toggleFullScreen;
+
+  const VideoControls({
+    required this.controller,
+    required this.isFullScreen,
+    required this.toggleFullScreen,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Positioned(
+          right: 0,
+          bottom: 0,
+          child: IconButton(
+            icon: Icon(
+              isFullScreen ? Icons.fullscreen_exit : Icons.fullscreen,
+              color: Colors.white,
+            ),
+            onPressed: toggleFullScreen,
+          ),
+        ),
+        Center(
+          child: IconButton(
+            icon: Icon(
+              controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              if (controller.value.isPlaying) {
+                controller.pause();
+              } else {
+                controller.play();
+              }
+            },
+          ),
+        ),
+      ],
     );
   }
 }
