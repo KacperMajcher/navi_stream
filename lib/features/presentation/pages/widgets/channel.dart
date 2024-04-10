b80956e08dde4256721bf0fa067c0e19416f1a13
kacpermajcher <kacpermajcher13@gmail.com>
2024-04-10 16:15:21 +0200

    Update look of Channel Widget

--- lib/features/presentation/pages/widgets/channel.dart
+++ lib/features/presentation/pages/widgets/channel.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:navi_stream/features/presentation/data/models/channel_model.dart';
+import 'package:navi_stream/features/presentation/pages/widgets/channel_player.dart';
 
 class Channel extends StatelessWidget {
   const Channel({
@@ -20,79 +21,48 @@ class Channel extends StatelessWidget {
           color: Color(0xFFC9CACF),
           thickness: 1,
         ),
-        Row(children: [
-          SizedBox(
-            width: dw * .25,
-            child: Image.network(
-              channelModel.channelLogo,
-              height: dh * 0.04,
-              width: dw * 0.1,
-              fit: BoxFit.contain,
-              // default image in case of an error loading the network image
-              errorBuilder: (context, exception, stackTrace) {
-                return Image.asset(
-                  'assets/channel_logo_test.jpg',
-                  height: dh * 0.055,
-                  width: dw * 0.1,
-                  fit: BoxFit.contain,
-                );
-              },
+        GestureDetector(
+          onTap: () => Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (_) => const ChannelPlayer(),
             ),
           ),
-          SizedBox(width: dw * .015),
-          Text(
-            channelModel.channelName,
-            style: const TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.w500,
-            ),
-          ),
-          const Spacer(),
-          Icon(
-            Icons.arrow_forward_ios_rounded,
-            size: dh * .02,
-            color: const Color(0xFF515257),
-          )
-        ]),
-      ],
-    );
-
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: dw * .03),
-      child: Card(
-        child: Row(
-          children: [
-            Padding(
-              padding: EdgeInsets.all(dh * 0.01),
+          child: Row(children: [
+            SizedBox(
+              width: dw * .25,
               child: Image.network(
                 channelModel.channelLogo,
-                height: dh * 0.055,
-                width: dw * 0.11,
+                height: dh * 0.04,
+                width: dw * 0.1,
                 fit: BoxFit.contain,
-
                 // default image in case of an error loading the network image
                 errorBuilder: (context, exception, stackTrace) {
                   return Image.asset(
                     'assets/channel_logo_test.jpg',
                     height: dh * 0.055,
-                    width: dw * 0.11,
+                    width: dw * 0.1,
                     fit: BoxFit.contain,
                   );
                 },
               ),
             ),
-            Expanded(
-              child: Text(
-                channelModel.channelName,
-                style: const TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w500,
-                ),
+            SizedBox(width: dw * .015),
+            Text(
+              channelModel.channelName,
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
               ),
             ),
-          ],
+            const Spacer(),
+            Icon(
+              Icons.arrow_forward_ios_rounded,
+              size: dh * .02,
+              color: const Color(0xFF515257),
+            )
+          ]),
         ),
-      ),
+      ],
     );
   }
 }
