5eb72c67e69d4ecf813ac14aefd407944478f1cf
kacpermajcher <kacpermajcher13@gmail.com>
2024-04-10 09:34:20 +0200

    Add base64 decoding in the application and display decrypted video

--- lib/features/presentation/pages/widgets/channel_player.dart
+++ lib/features/presentation/pages/widgets/channel_player.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:navi_stream/core/constants/constants.dart';
 import 'package:video_player/video_player.dart';
 
 class ChannelPlayer extends StatefulWidget {
@@ -8,14 +13,28 @@ class ChannelPlayer extends StatefulWidget {
   ChannelPlayerState createState() => ChannelPlayerState();
 }
 
+String decodedUrl = utf8.decode(base64.decode(playerBase64));
+final url = Uri.parse(kIsWeb
+    ? 'https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4'
+    : decodedUrl);
+
+/*
+
+D E C O D I N G
+
+The web does not support the decoded link format.
+When opening in the browser, display the link in .mp4 format.
+In the case of a mobile application, display the decoded video.
+
+*/
+
 class ChannelPlayerState extends State<ChannelPlayer> {
   late VideoPlayerController _controller;
 
   @override
   void initState() {
     super.initState();
-    _controller = VideoPlayerController.networkUrl(Uri.parse(
-        'https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4'));
+    _controller = VideoPlayerController.networkUrl(url);
 
     _controller.addListener(() {
       setState(() {});
@@ -23,6 +42,11 @@ class ChannelPlayerState extends State<ChannelPlayer> {
     _controller.setLooping(true);
     _controller.initialize().then((_) => setState(() {}));
     _controller.play();
+
+    log(kIsWeb
+        ? 'Welcome in the web, I\'ll play a link with a browser-supported format.'
+        : 'Welcome to the app, I have a specially decoded video for you.');
+        
   }
 
   @override
