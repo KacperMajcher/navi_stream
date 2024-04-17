d516b8ea22daf06abd00ce0b94ef9ce7e647133d
kacpermajcher <kacpermajcher13@gmail.com>
2024-04-17 19:57:59 +0200

    Test HomeState

--- test/features/home/pages/cubit/home_state_test.dart
+++ test/features/home/pages/cubit/home_state_test.dart
@@ -1,40 +1,85 @@
+import 'dart:convert';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
-import 'package:navi_stream/features/home/data/repositories/channel_repository.dart';
-import 'package:navi_stream/features/home/data/repositories/packages_repository.dart';
-import 'package:navi_stream/features/home/pages/cubit/home_cubit.dart';
-import 'package:navi_stream/features/home/pages/cubit/home_state.dart';
 import 'package:navi_stream/core/constants/enums.dart';
+import 'package:navi_stream/features/home/data/models/channel_model.dart';
+import 'package:navi_stream/features/home/pages/cubit/home_state.dart';
 
-class MockPackagesRepository extends Mock implements PackagesRepository {}
-
-class MockChannelsRepository extends Mock implements ChannelsRepository {}
+class MockChannelModel extends Mock implements ChannelModel {}
 
 void main() {
-  late MockPackagesRepository mockPackagesRepository;
-  late MockChannelsRepository mockChannelsRepository;
-  late HomeCubit sut;
+  final jsonMap = {
+    'status': 'loading',
+    'channelModel': [
+      {
+        'channelName': 'Channel 1',
+        'logoId': 1,
+        'channelLogo': 'logo 1',
+      },
+      {
+        'channelName': 'Channel 2',
+        'logoId': 2,
+        'channelLogo': 'logo 2',
+      },
+    ],
+  };
 
-  setUp(
-    () {
-      mockPackagesRepository = MockPackagesRepository();
-      mockChannelsRepository = MockChannelsRepository();
-      sut = HomeCubit(
-          packagesRepository: mockPackagesRepository,
-          channelsRepository: mockChannelsRepository);
-    },
-  );
+  final expectedChannelModels = [
+    ChannelModel(
+      channelName: 'Channel 1',
+      logoId: 1,
+      channelLogo: 'logo 1',
+    ),
+    ChannelModel(
+      channelName: 'Channel 2',
+      logoId: 2,
+      channelLogo: 'logo 2',
+    ),
+  ];
 
   group(
-    'HoginState tests',
+    'HomeState tests',
     () {
       test(
-        'initial state is HomeState.loading',
+        'fromJson should correctly deserialize JSON',
         () {
+          final state = HomeState.fromJson(jsonMap);
+
           expect(
-            sut.state,
-            const HomeState(
-              status: Status.loading,
+            state.status,
+            equals(Status.loading),
+          );
+
+          expect(
+            state.channelModel,
+            equals(expectedChannelModels),
+          );
+        },
+      );
+
+      test(
+        'toJson should correctly serialize to JSON',
+        () {
+          final state = HomeState(
+            status: Status.loading,
+            channelModel: expectedChannelModels,
+          );
+
+          final jsonMap = state.toJson();
+
+          final List<Map<String, dynamic>> expectedChannelModelMaps = state
+              .channelModel
+              .map((channelModel) => channelModel.toJson())
+              .toList();
+
+          expect(jsonMap, jsonMap);
+          expect(
+            json.encode(jsonMap['channelModel']),
+            equals(
+              json.encode(
+                expectedChannelModelMaps,
+              ),
             ),
           );
         },
