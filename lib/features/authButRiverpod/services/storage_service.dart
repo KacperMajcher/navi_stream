0f538792c2882445fab44e4b001865e04e858d08
kacpermajcher <kacpermajcher13@gmail.com>
2024-04-28 16:53:33 +0200

    Create storage service

---
+++ lib/features/authButRiverpod/services/storage_service.dart
@@ -0,0 +1,35 @@
+import 'package:injectable/injectable.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+@injectable
+class StorageService {
+  Future<bool> setToken(response) async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.setString('token', response.data['data']['access_token']);
+  }
+
+  Future<bool> setOuid(response) async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.setString('ouid', response.data['data']['operator_uid']);
+  }
+
+  Future<bool> setUserId(response) async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.setInt('userId', response.data['data']['user_id']);
+  }
+
+  Future<String> getToken() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString('token')!;
+  }
+
+  Future<String> getOuid() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString('ouid')!;
+  }
+
+  Future<int> getUserId() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getInt('userId') as int;
+  }
+}
